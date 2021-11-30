21000ff3c3473a7709bd57d8de013899c98ba364
Andrew Mackay <andrewmackayd@gmail.com>
2021-11-30 16:40:51 +0000

    AM, 2nd commit main, set up back-end

---
+++ project_management_back_end/src/main/java/com/codeclan/example/project_management_back_end/ProjectManagementBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.project_management_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementBackEndApplication.class, args);
+	}
+
+}
---
+++ project_management_back_end/src/test/java/com/codeclan/example/project_management_back_end/ProjectManagementBackEndApplicationTests.java
@@ -0,0 +1,14 @@
+
+package com.codeclan.example.project_management_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
