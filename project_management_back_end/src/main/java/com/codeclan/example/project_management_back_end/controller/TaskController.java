73b9f4fbc6ae83537f4761d144b2ebe6ba655d7b
Andrew Mackay <andrewmackayd@gmail.com>
2021-12-02 18:15:39 +0000

    AM, 12th commit main, back-end

--- project_management_back_end/src/main/java/com/codeclan/example/project_management_back_end/controller/TaskController.java
+++ project_management_back_end/src/main/java/com/codeclan/example/project_management_back_end/controller/TaskController.java
@@ -2,6 +2,7 @@ package com.codeclan.example.project_management_back_end.controller;
 
 import com.codeclan.example.project_management_back_end.models.Task;
 import com.codeclan.example.project_management_back_end.repositories.TaskRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,18 +23,25 @@ public class TaskController {
 
     @GetMapping(value = "/tasks/{id}")
     public ResponseEntity getTask(@PathVariable Long id){
-        return new ResponseEntity<>(taskRepository.findById(id), HttpStatus.OK)
+        return new ResponseEntity<>(taskRepository.findById(id), HttpStatus.OK);
     }
 
     @PostMapping(value = "/tasks")
     public ResponseEntity<Task> postTask(@RequestBody Task task){
         taskRepository.save(task);
-        return new ResponseEntity<>(pirate, HttpStatus.CREATED);
+        return new ResponseEntity<>(task, HttpStatus.CREATED);
     }
 
     @PatchMapping(value = "/tasks/{id}")
     public ResponseEntity<Task> updateTask(@RequestBody Task task){
         taskRepository.save(task);
         return new ResponseEntity<>(task, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/tasks/{id}")
+    public ResponseEntity<Task> deleteTask(@PathVariable Long id) {
+        Task found = taskRepository.getOne(id);
+        taskRepository.delete(found);
+        return new ResponseEntity<>(null, HttpStatus.OK);
     }
 }
