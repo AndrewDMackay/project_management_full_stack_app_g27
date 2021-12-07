4e072719d5458b0ebde04c449698a11227a777b7
Andrew Mackay <andrewmackayd@gmail.com>
2021-12-07 14:59:09 +0000

    AM, 42nd commit main, front-end

--- project_management_front_end/src/components/boards/BoardList.js
+++ project_management_front_end/src/components/boards/BoardList.js
@@ -1,43 +1,42 @@
 import React, { useState } from 'react'
 
 
-const BoardList = ({boards}) => {
+const BoardList = ({ boards }) => {
 
-    
+    if (boards.length === 0) {
+        return (
+            <div className="loading-message-container">
+                <p>Loading..</p>
+            </div>
+
+        )
+    }
+
+    const boardsNodes = boards.map((board, index) => {
+        return (
+            <li key={index} className="board-unordered-list-li">
+                <div>
+                    <Board board={board} />
+                </div>
+            </li>
+        )
+    })
 
     return (
         <>
             <div className="board-list-container">
                 <h1>Board List</h1>
                 <p>I am the Board List..</p>
+                <div>
+                    <ul className="board-unordered-list">
+                        {boardsNodes}
+                    </ul>
+                </div>
             </div>
         </>
     )
-
 }
 
-// const PirateList = ({pirates}) => {
-
-// 	if (pirates.length === 0){
-// 	  return (<p>Loading...</p>)
-// 	}
-
-// 	const piratesNodes = pirates.map((pirate, index) => {
-// 	  return (
-// 	    <li key={index} className="component-item">
-// 	    <div className="component">
-// 	    <Pirate pirate={pirate} />
-// 	    </div>
-// 	    </li>
-// 	  )
-// 	})
-
-// 	return (
-// 	  <ul className="component-list">
-// 	    {piratesNodes}
-// 	  </ul>
-// 	)
-// }
 
 export default BoardList;
 
