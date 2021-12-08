6d2e333cfba98972cdd6acefcfea172b156d0574
Andrew Mackay <andrewmackayd@gmail.com>
2021-12-08 10:18:13 +0000

    AM, 65th commit main, front-end

--- project_management_front_end/src/components/tasks/TaskForm.js
+++ project_management_front_end/src/components/tasks/TaskForm.js
@@ -1,19 +1,54 @@
 import React, { useState, useEffect } from 'react';
 
 
-const TaskForm = () => {
+const TaskForm = ({ task, onNewTaskSubmit }) => {
+
+    const [date, setDate] = useState("")
+    const [time, setTime] = useState("")
+    const [comment, setComment] = useState("")
+    const [priority, setPriority] = useState("")
+    const [completed, setCompleted] = useState("")
+
+    // Loading message..
+
+    if (!task) {
+        <div className="loading-message-container">
+            <p>Loading..</p>
+        </div>
+    }
+
+    // Task form functions.. 
+
+    const handleNameChange = (event) => {
+        setName(event.target.value)
+    }
+
+    const handleCommentChange = (event) => {
+        setComment(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const newTask = {
+            name: name,
+            comment: comment
+        }
+        onNewTaskSubmit(newTask);
+    }
 
     return (
         <>
             <div className="task-form-container">
-                <h1>Task Form</h1>
-                <p>I am the Task Form..</p>
+                <h1>New Task</h1>
+                <form className="form-container" onSubmit={handleSubmit}>
+                    <input className="form-inputs" type="text" id="name" placeholder="Enter Name.." value={name} onChange={handleNameChange} required />
+                    <input className="form-inputs" type="text" id="comment" placeholder="Enter Comment.." value={comment} onChange={handleCommentChange} required />
+                    <input type="submit" value="submit" />
+                </form>
             </div>
         </>
     )
-
 }
-
 
 export default TaskForm;
 
--- project_management_front_end/src/containers/BoardContainer.js
+++ project_management_front_end/src/containers/BoardContainer.js
@@ -67,7 +67,6 @@ const BoardContainer = () => {
             })
     }
 
-
     if (!boards) {
         return null;
     }
